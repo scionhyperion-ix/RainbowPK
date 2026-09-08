cb925e21d2b543a986a11e8261c699d2246ced8f
Jonathan Crane <jonathancraneofficial@gmail.com>
2026-09-08 23:40:32 +0800

    Cache group member access assets

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'rainbow-shell-v15';
+const CACHE_NAME = 'rainbow-shell-v16';
 const APP_SHELL = [
   './',
   './index.html',
@@ -11,6 +11,7 @@ const APP_SHELL = [
   './assets/css/history-behavior.css?v=1',
   './assets/css/member-editor-v2.css?v=1',
   './assets/css/groups-route.css?v=1',
+  './assets/css/groups-members-access.css?v=1',
   './assets/css/guide-popovers.css?v=3',
   './assets/css/member-view-menu.css?v=1',
   './assets/css/modal-scroll-lock.css?v=1',
@@ -36,6 +37,7 @@ const APP_SHELL = [
   './assets/js/member-editor-v2.js?v=1',
   './assets/js/groups-route.js?v=1',
   './assets/js/groups-route-runtime.js?v=1',
+  './assets/js/groups-members-access.js?v=1',
   './assets/js/guide-popovers.js?v=3',
   './assets/js/member-view-menu.js?v=1',
   './assets/js/modal-scroll-lock.js?v=1',
