c6f5dfd8f0bf35870b64b8c75771d3a2304115a3
Jonathan Crane <jonathancraneofficial@gmail.com>
2026-09-09 01:28:34 +0800

    Refresh Members theme surface

--- assets/js/site-features-runtime.js
+++ assets/js/site-features-runtime.js
@@ -37,7 +37,7 @@
   ensureStylesheet('assets/css/home-system-controls.css?v=2');
   ensureStylesheet('assets/css/multi-cofronter.css?v=1');
   ensureStylesheet('assets/css/guide-popovers.css?v=3');
-  ensureStylesheet('assets/css/member-view-menu.css?v=1');
+  ensureStylesheet('assets/css/member-view-menu.css?v=2');
   ensureStylesheet('assets/css/modal-scroll-lock.css?v=1');
   loadScript('assets/js/history-behavior.js?v=1');
   loadScript('assets/js/member-editor-v2.js?v=1');
