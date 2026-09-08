d13a4e35100862152dab067160c15c312a87f8b3
Jonathan Crane <jonathancraneofficial@gmail.com>
2026-09-09 00:48:05 +0800

    Load home system controls

--- assets/js/site-features-runtime.js
+++ assets/js/site-features-runtime.js
@@ -34,6 +34,7 @@
   ensureStylesheet('assets/css/groups-route.css?v=2');
   ensureStylesheet('assets/css/groups-members-access.css?v=3');
   ensureStylesheet('assets/css/avatar-rendering.css?v=1');
+  ensureStylesheet('assets/css/home-system-controls.css?v=1');
   ensureStylesheet('assets/css/guide-popovers.css?v=3');
   ensureStylesheet('assets/css/member-view-menu.css?v=1');
   ensureStylesheet('assets/css/modal-scroll-lock.css?v=1');
@@ -42,6 +43,7 @@
   loadScript('assets/js/groups-route.js?v=1');
   loadScript('assets/js/groups-route-runtime.js?v=1');
   loadScript('assets/js/groups-members-access.js?v=2');
+  loadScript('assets/js/home-system-controls.js?v=1');
   loadScript('assets/js/guide-popovers.js?v=3');
   loadScript('assets/js/member-view-menu.js?v=1');
   loadScript('assets/js/modal-scroll-lock.js?v=1');
