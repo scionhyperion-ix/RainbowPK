972afee614541549f31e617ff95e7e1b9c3b4674
Jonathan Crane <jonathancraneofficial@gmail.com>
2026-09-09 01:59:32 +0800

    Cache Current Front stability patch

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'rainbow-shell-v29';
+const CACHE_NAME = 'rainbow-shell-v30';
 const APP_SHELL = [
   './',
   './index.html',
@@ -45,6 +45,7 @@ const APP_SHELL = [
   './assets/js/groups-route-runtime.js?v=1',
   './assets/js/groups-members-access.js?v=2',
   './assets/js/home-system-controls.js?v=1',
+  './assets/js/front-scroll-stability.js?v=1',
   './assets/js/multi-cofronter.js?v=1',
   './assets/js/guide-popovers.js?v=3',
   './assets/js/member-view-menu.js?v=1',
