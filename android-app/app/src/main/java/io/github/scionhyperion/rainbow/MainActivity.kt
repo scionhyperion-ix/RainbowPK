3cf96fd7e55a704dbd02cd04587dc32358be01f3
Jonathan Crane <jonathancraneofficial@gmail.com>
2026-09-08 19:14:25 +0800

    Polish native Android app shell

--- android-app/app/src/main/java/io/github/scionhyperion/rainbow/MainActivity.kt
+++ android-app/app/src/main/java/io/github/scionhyperion/rainbow/MainActivity.kt
@@ -1,7 +1,10 @@
 package io.github.scionhyperion.rainbow
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.webkit.JavascriptInterface
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
@@ -15,20 +18,49 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        window.statusBarColor = Color.parseColor("#0B0C16")
+        window.navigationBarColor = Color.parseColor("#0B0C16")
+
         webView = WebView(this)
+        webView.setBackgroundColor(Color.parseColor("#0B0C16"))
+        webView.overScrollMode = View.OVER_SCROLL_NEVER
+        webView.isVerticalScrollBarEnabled = false
+        webView.isHorizontalScrollBarEnabled = false
         setContentView(webView)
 
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
         webView.settings.allowFileAccess = false
         webView.settings.allowContentAccess = false
+        webView.settings.setSupportZoom(false)
+        webView.settings.builtInZoomControls = false
+        webView.settings.displayZoomControls = false
+        webView.settings.userAgentString = "${webView.settings.userAgentString} RainbowAndroid/0.2"
+
         webView.addJavascriptInterface(AndroidBridge(), "RainbowAndroid")
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
                 val uri = request.url
-                return uri.host != "scionhyperion-ix.github.io"
+                if (uri.host == "scionhyperion-ix.github.io") return false
+
+                return try {
+                    startActivity(Intent(Intent.ACTION_VIEW, uri))
+                    true
+                } catch (_: Exception) {
+                    true
+                }
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                super.onPageFinished(view, url)
+                view.evaluateJavascript(
+                    "document.documentElement.classList.add('native-app');" +
+                        "if(document.body)document.body.classList.add('native-app');",
+                    null
+                )
             }
         }
+
         webView.loadUrl("https://scionhyperion-ix.github.io/RainbowPK/")
     }
 
