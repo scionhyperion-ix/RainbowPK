aaa8a730ebab68d70f10e540da265facd19008c1
Jonathan Crane <jonathancraneofficial@gmail.com>
2026-09-09 01:54:42 +0800

    Refresh viewport theme shell

--- assets/js/site-features-runtime.js
+++ assets/js/site-features-runtime.js
@@ -29,7 +29,7 @@
     document.body.append(script);
   }
 
-  ensureStylesheet('assets/css/app-theme-shell.css?v=1');
+  ensureStylesheet('assets/css/app-theme-shell.css?v=2');
   ensureStylesheet('assets/css/history-behavior.css?v=1');
   ensureStylesheet('assets/css/member-editor-v2.css?v=1');
   ensureStylesheet('assets/css/groups-route.css?v=2');
