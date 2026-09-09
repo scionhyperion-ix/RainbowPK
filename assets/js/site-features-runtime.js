b02565afb9961398fef85e2130af0bc2132c1a2f
Jonathan Crane <jonathancraneofficial@gmail.com>
2026-09-09 12:42:30 +0800

    Load mobile Members audit styles

--- assets/js/site-features-runtime.js
+++ assets/js/site-features-runtime.js
@@ -41,6 +41,7 @@
   ensureStylesheet('assets/css/guide-popovers.css?v=3');
   ensureStylesheet('assets/css/member-view-menu.css?v=3');
   ensureStylesheet('assets/css/mobile-member-views.css?v=1');
+  ensureStylesheet('assets/css/mobile-members-audit.css?v=1');
   ensureStylesheet('assets/css/modal-scroll-lock.css?v=1');
   loadScript('assets/js/history-behavior.js?v=1');
   loadScript('assets/js/member-editor-v2.js?v=1');
